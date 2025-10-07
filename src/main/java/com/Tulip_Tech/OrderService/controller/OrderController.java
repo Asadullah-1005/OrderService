30c4791f8367ef09b21537b2555231327b234032
Asadullah-1005 <aullah@tulip-tech.com>
2025-10-07 19:23:06 +0600

    Add tracing use zipkin, opentelemetry, micrometer.

--- src/main/java/com/Tulip_Tech/OrderService/controller/OrderController.java
+++ src/main/java/com/Tulip_Tech/OrderService/controller/OrderController.java
@@ -5,10 +5,7 @@ import com.Tulip_Tech.OrderService.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("order")
@@ -22,6 +19,11 @@ public class OrderController {
     @PostMapping("/placeOrder")
     public ResponseEntity<?> placeOrder(@RequestBody CreateOrderRequest createOrderRequest) {
         return orderService.placeOrder(createOrderRequest);
+    }
+
+    @GetMapping("/check")
+    public String healthCheck(){
+        return "Order Service is up and running";
     }
 
 }
